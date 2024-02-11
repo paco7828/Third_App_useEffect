c6b65b075cf5efc2f7a4a13a0904b90a139bce8b
paco7828 <koszegipatrik888@gmail.com>
2024-02-11 14:36:32 +0100

    Final commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Comp() {
+    const [text, setText] = useState("Don't change this input's value!");
+    const [inputValue, setInputValue] = useState("72548125636");
+    const [textFontSize, setTextFontSize] = useState(12);
+    const [index, setIndex] = useState(-1);
+    const angerColors = ["green", "blue", "pink", "purple", "red"];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const handleChange = (e) => {
+        setInputValue(e.target.value);
+        setText("Why are you changing the value???");
+        setTextFontSize(prevSize => prevSize + 4);
+    }
+
+    useEffect(() => {
+        if (index === 4) {
+            return () => { };
+        }
+        setIndex(prevIndex => prevIndex + 1);
+    }, [textFontSize])
+
+    return (
+        <form>
+            <h1 style={{ fontSize: textFontSize }}>{text}</h1>
+            <input type="text" value={inputValue} onChange={handleChange} />
+            <h2>Anger level</h2>
+            <div style={{ width: "200px", height: "200px", backgroundColor: angerColors[index] }}></div>
+        </form>
+    );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Comp />);
